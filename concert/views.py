58718cf5eb5686afdbdc65d2f47d9c96c0471db6
Ervan <ervan.a.haryadi@gmail.com>
2024-07-18 21:33:07 +0700

    create signup url and view function

--- concert/views.py
+++ concert/views.py
@@ -14,7 +14,23 @@ import requests as req
 # Create your views here.
 
 def signup(request):
-    pass
+    if request.method == 'POST':
+        username = request.POST.get['username']
+        password = request.POST.get['password']
+        try:
+            user = User.objects.filter(username=username).first()
+            if user:
+                return render(request, 'signup.html', context={'form':SignUpForm, "message":"user already exists"})
+            else:
+                user = User.objects.create(
+                    username=username,
+                    password=make_password(password)
+                )
+                login(request, user)
+                return HttpResponseRedirect(reverse("index"))
+        except User.DoesNotExist:
+            return render(request, 'signup.html', {'form':SignUpForm})
+    return render(request, 'signup.html', {'form':SignUpForm})
 
 
 def index(request):
